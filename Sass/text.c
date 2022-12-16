8768ca60311679fc4d9826a5be6c3fde08cc6bd0
abdelaziz ez-zahed <83925203+AZdevloper@users.noreply.github.com>
2022-12-16 20:47:49 +0100

    Add files via upload
    add fille sass revise

---
+++ Sass/text.c
@@ -0,0 +1,9 @@
+// sass : Syntactically Awesome Stylesheet.
+// is a powerful CSS preprocessor (generate css style) scripting language that helps you to
+// work on your style sheet much faster than ever.
+// Sass allows you to use features such as variables, function
+// used to make dark modes
+// static to dynamic language
+// used to creat css library
+// css prefix (-webkit-,-moz-) is are a way for browser makers to add support for new CSS features before those 
+    //features are fully supported in all browsers
